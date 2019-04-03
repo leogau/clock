d303708ae50918f67db8024ecae49a1647da6597
Leo Gau <leo@Megans-iMac.local>
2019-04-03 13:06:48 -0700

    initial commit

---
+++ Assets/Clock.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+
+    public bool continuous;
+
+    public void Update()
+    {
+        if (continuous)
+        {
+            UpdateContinuous();
+        }
+        else
+        {
+            UpdateDiscrete();
+        }
+    }
+
+    private void UpdateContinuous()
+    {
+        const float degreesPerHour = 30f;
+        const float degreesPerMinute = 6f;
+        const float degreesPerSecond = 6f;
+
+        var now = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation = Quaternion.Euler(0f, (float)now.TotalHours * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, (float)now.TotalMinutes * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, (float)now.TotalSeconds * degreesPerSecond, 0f);
+    }
+
+    private void UpdateDiscrete()
+    {
+        const float degreesPerHour = 30f;
+        const float degreesPerMinute = 6f;
+        const float degreesPerSecond = 6f;
+
+        DateTime now = DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0f, now.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, now.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, now.Second * degreesPerSecond, 0f);
+    }
+}
